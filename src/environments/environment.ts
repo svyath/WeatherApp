7cc32da69ec221e20b098eb63b53ebc7272f867d
Svyatyk <43937124+Svyatyk@users.noreply.github.com>
2020-11-30 00:52:24 +0200

    W-App v. 1.0.1.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,3 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
   production: false,
   appID: '1:450428813764:web:5b6bd7ee4e29fd33032491',
@@ -15,10 +11,4 @@ export const environment = {
   }
 };
 
-/*
- * In development mode, to ignore zone related error stack frames such as
- * `zone.run`, `zoneDelegate.invokeTask` for easier debugging, you can
- * import the following file, but please comment it out in production mode
- * because it will have performance impact when throw error
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+
